cd4e262a04c07c2dc36b099a447ae0cc7960529d
Armanidrisi <idrisiarman17@gmail.com>
2023-04-14 21:13:09 +0530

    project 10 completed

---
+++ project-10_404_error_page/js/index.js
@@ -0,0 +1,5 @@
+//For Go Back Button : (It's Optional You Can Also Use Anchor Tag Of Html)
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+  window.location.href="/"
+});
